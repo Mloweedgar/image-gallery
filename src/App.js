482221584a34b81cc3a47ccae62fba7bba4a8741
Mloweedgar <mloweedgar0@gmail.com>
2018-04-04 20:03:26 +0300

    added sample images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Cache gallery container
+const galleryContainer = document.querySelector('.react-gallery');
+
+// Create new array with URLs for images
+let imgUrls = [
+    'https://source.unsplash.com/3Z70SDuYs5g/800x600',
+    'https://source.unsplash.com/01vFmYAOqQ0/800x600',
+    'https://source.unsplash.com/2Bjq3A7rGn4/800x600',
+    'https://source.unsplash.com/t20pc32VbrU/800x600',
+    'https://source.unsplash.com/pHANr-CpbYM/800x600',
+    'https://source.unsplash.com/3PmwYw2uErY/800x600',
+    'https://source.unsplash.com/uOi3lg8fGl4/800x600',
+    'https://source.unsplash.com/CwkiN6_qpDI/800x600',
+    'https://source.unsplash.com/9O1oQ9SzQZQ/800x600',
+    'https://source.unsplash.com/E4944K_4SvI/800x600',
+    'https://source.unsplash.com/-hI5dX2ObAs/800x600',
+    'https://source.unsplash.com/vZlTg_McCDo/800x600'
+];
 
 class App extends Component {
 
@@ -9,7 +27,7 @@ class App extends Component {
 
       let title = 'Hello world';
     return (
-      <h1>{title}</h1>
+      <h1 className="jumbotron">{title}</h1>
     );
   }
 }
