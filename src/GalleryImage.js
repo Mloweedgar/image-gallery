bab9b8f43a2f9abc84dbbf4ec9c67b97017aec9e
Mloweedgar <mloweedgar0@gmail.com>
2018-04-04 20:18:01 +0300

    gallery image component

---
+++ src/GalleryImage.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from 'react';
+
+// Generates img elements for the gallery
+class GalleryImage extends Component {
+    render() {
+        return(
+            <img className={this.props.className} src={this.props.src} alt={this.props.alt} />
+        )
+    }
+}
