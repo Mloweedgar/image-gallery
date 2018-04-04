09917fbe3c4234f9cccb650d605fb5b9bdfb1dae
Mloweedgar <mloweedgar0@gmail.com>
2018-04-04 19:49:33 +0300

    setting up react project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+
+  render() {
+
+      let title = 'Hello world';
+    return (
+      <h1>{title}</h1>
+    );
+  }
+}
+
+export default App;
